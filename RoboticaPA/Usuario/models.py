63da6e19a9a904490b9a2eb142ccce369179176f
Carla Fernandes <carlafcf@gmail.com>
2021-11-17 16:37:10 -0300

    Criação do MODEL Usuario

--- RoboticaPA/Usuario/models.py
+++ RoboticaPA/Usuario/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.RESTRICT)
+    cidade = models.CharField(max_length=100, verbose_name='Cidade')
+    estado = models.CharField(max_length=100, verbose_name='Estado')
+
+    def __str__(self):
+        return self.usuario.first_name + " " + self.usuario.last_name
+
+    class Meta:
+        ordering = ['usuario']
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
