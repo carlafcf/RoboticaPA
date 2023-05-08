acf8296a7c2921de65cd3603cb2f5a2a9744e0f9
Aylane Santos <aylanesantos9@gmail.com>
2023-05-08 18:39:22 -0300

    Salvando listar

---
+++ RoboticaPA/PlanoAula/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from PlanoAula.models import PlanoAula, LikePlanoAula, ExecucaoPlanoAula
+
+admin.site.register(PlanoAula)
+admin.site.register(LikePlanoAula)
+admin.site.register(ExecucaoPlanoAula)
