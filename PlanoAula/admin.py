404d65a5ac5edc8baf082b6c5863c18ea7f1cc76
Carla Fernandes <carlafcf@gmail.com>
2023-07-12 22:19:58 -0300

    Ajustando correção de pastas

--- PlanoAula/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from PlanoAula.models import PlanoAula, LikePlanoAula, ExecucaoPlanoAula
-
-admin.site.register(PlanoAula)
-admin.site.register(LikePlanoAula)
-admin.site.register(ExecucaoPlanoAula)
--- Usuario/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from Usuario.models import Usuario, Interesses
-
-admin.site.register(Usuario)
-admin.site.register(Interesses)
